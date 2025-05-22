8bb4087c112add7db200f9235f6c253ea4e11105
Dylan <dylan.calle@ucb.edu.bo>
2025-05-21 20:56:34 -0400

    Se agregaron nuevas lineas de codigo

--- src/totalizador.spec.js
+++ src/totalizador.spec.js
@@ -5,4 +5,13 @@ describe("Mostrar Items", () => {
     it("Deberia mostrarse 5", () => {
         expect(showCant(5)).toEqual(5); 
     });
+    it("Deberia mostrar el numero ingresado", () => {
+        expect(showCant(20)).toEqual(20); 
+    });
+    it("Deberia mostrar el numero ingresado", () => {
+        expect(showCant(20)).toEqual(20); 
+    });
+    it("Deberia mostrar el numero ingresado", () => {
+        expect(showCant(20)).toEqual(20); 
+    });
 });
